7621b9cc833ab605f2ea0268082a7c991e32fd84
UnseeN <nca05531@gmail.com>
2020-12-27 21:54:16 +0630

    Django-Custom-User-Model

---
+++ README.md
@@ -0,0 +1,6 @@
+Django-Custom-User-Model
+
+Usage:
+-python3 manage.py makemigrations
+-python3 manage.py migrate
+-python3 manage.py runserver
---
+++ users/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'),unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # provide additional fields when we run [createsuperuser] command
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+#AbstractBaseUser
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractBaseUser
+# from django.contrib.auth.models import PermissionsMixin
+# from django.utils.translation import gettext_lazy as _
+# from django.utils import timezone
+
+# from .managers import CustomUserManager
+
+
+# class CustomUser(AbstractBaseUser,PermissionsMixin):
+#     email = models.EmailField(_('email address'), unique=True)
+#     is_staff = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=True)
+#     date_joined = models.DateTimeField(default=timezone.now)
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+
+#     objects = CustomUserManager()
+
+#     def __str__(self):
+#         return self.email
